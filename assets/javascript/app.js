51d1bf6e1005d3edd0246870a14200bf2a5fed44
Rayal <rayalsgit@users.noreply.github.com>
2017-02-16 17:09:56 -0500

    added comments

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,3 @@
-/* global firebase moment */
-// Steps to complete:
-
-// 1. Initialize Firebase
-// 2. Create button for adding new employees - then update the html + update the database
-// 3. Create a way to retrieve employees from the employee database.
-// 4. Create a way to calculate the months worked. Using difference between start and current time.
-//    Then use moment.js formatting to set difference in months.
-// 5. Calculate Total billed
-
-
-
 // Initialize Firebase
 var config = {
     apiKey: "AIzaSyD_lHm39Rj-aH5mOk_daFmy0bmRJvpqowI",
@@ -20,13 +8,13 @@ var config = {
 };
 firebase.initializeApp(config);
 
-
 var database = firebase.database();
 
 // 2. Button for adding trains
 $("#add-train-btn").on("click", function(event) {
     event.preventDefault();
     var key = "";
+    //indentify if it is update / insert
     if ($(this).html() === "Update") {
         $(this).html("Submit");
         key = $(this).attr("data-tr");
@@ -46,7 +34,7 @@ $("#add-train-btn").on("click", function(event) {
         frequency: frequency,
         fttime: fttime
     };
-
+    // if new train
     if (key.length === 0) {
         // Uploads new train data to the database
         database.ref().push(train);
@@ -71,7 +59,7 @@ $("#add-train-btn").on("click", function(event) {
 // 3. Create Firebase event for adding a row in the html when a user adds an entry
 database.ref().on("child_added", function(record, prevChildKey) {
 
-    console.log(record.val());
+    // get snapshot key
     var trainid = record.key;
 
     // Store everything into a variable.
@@ -79,24 +67,24 @@ database.ref().on("child_added", function(record, prevChildKey) {
     var dest = record.val().dest;
     var frequency = record.val().frequency;
     var st = record.val().fttime;
+    //convert to military time
     var fttime = moment(st, "HH:mm");
-
+    //get current time
     var endTime = moment();
-
+    // difference between both times in mins
     var mins = endTime.diff(fttime, "minutes");
-    console.log("Diff in mins: " + mins);
 
     var minSinceLast = mins % frequency;
     var minsRemaining = frequency - minSinceLast;
+
     var nextArrival = moment().add(minsRemaining, "minutes").format("HH:mm");
-    console.log("Next Arrival In: " + minsRemaining + " mins");
-    console.log(nextArrival);
+
 
     // Add each train's data into the table
     $("#train-table > tbody").append("<tr class='train_tr' data-st='" + st + "' data-id='" + trainid + "'><td>" + trainName + "</td><td>" + dest + "</td><td class='fr'>" +
         frequency + "</td><td class='na'>" + nextArrival + "</td><td class='mr'>" + minsRemaining + "</td><td><button class='btn btn-warning upd' type='button' onClick='handleUpdate()'>Edit</button></td><td><button class='btn btn-danger del' type='button' onClick='handleDelete()'>Delete</button></td></tr>");
 });
-
+//handler to update table when a train is deleted
 database.ref().on("child_removed", function(record) {
     var trainid = record.key;
     $(".train_tr").filter(function() {
@@ -104,21 +92,24 @@ database.ref().on("child_removed", function(record) {
     }).remove();
 });
 
+//handler to update table when a train is modified
 database.ref().on("child_changed", function(record) {
     var trainid = record.key;
     var trainName = record.val().name;
     var dest = record.val().dest;
     var frequency = record.val().frequency;
     var st = record.val().fttime;
+    //convert to military time
     var fttime = moment(st, "HH:mm");
-
+    //get current time
     var endTime = moment();
-
+    // cal difference in mins
     var mins = endTime.diff(fttime, "minutes");
 
     var minSinceLast = mins % frequency;
     var minsRemaining = frequency - minSinceLast;
     var nextArrival = moment().add(minsRemaining, "minutes").format("HH:mm");
+    // change the tr's innerHtml
     var content = "<td>" + trainName + "</td><td>" + dest + "</td><td class='fr'>" +
         frequency + "</td><td class='na'>" + nextArrival + "</td><td class='mr'>" + minsRemaining + "</td><td><button class='btn btn-warning upd' type='button' onClick='handleUpdate()'>Edit</button></td><td><button class='btn btn-danger del' type='button' onClick='handleDelete()'>Delete</button></td>";
     $(".train_tr").filter(function() {
@@ -126,59 +117,67 @@ database.ref().on("child_changed", function(record) {
     }).html(content);
 });
 
+// update times on table for every minute
 var timer = setInterval(function() {
-
+    // get all the tr's in the table
     var rows = document.getElementsByClassName("train_tr");
+    // loop for each row
     for (var i = 0; i < rows.length; i++) {
+        // capture each train details
         var tr = rows[i];
         var st = tr.getAttribute("data-st");
         var fttime = moment(st, "HH:mm");
         var e = tr.children[2];
         var frequency = parseInt(e.innerText);
         var endTime = moment();
-
+        // calcuate time diff in mins
         var mins = endTime.diff(fttime, "minutes");
-        console.log("Diff in mins: " + mins);
 
         var minSinceLast = mins % frequency;
         var minsRemaining = frequency - minSinceLast;
+        //calculate next arrival
         var nextArrival = moment().add(minsRemaining, "minutes").format("HH:mm");
-
+        //update the NextArrival and MinsRemaining column
         tr.children[3].innerText = nextArrival;
         tr.children[4].innerText = minsRemaining;
     }
 
 }, 60000);
 
-
+//handler for delete btn click event
 function handleDelete() {
     event.preventDefault();
-
+    // get the train-key from the tr 
     var newPostKey = event.target.parentNode.parentNode.getAttribute("data-id");
-
+    // create empty update rec
     var updates = {};
     updates[newPostKey] = null;
-
+    // delete from firebase 
     return database.ref().update(updates);
 };
 
+//handler for edit btn click event
 function handleUpdate() {
     event.preventDefault();
+    //get key from the Update button's data attrib
     var newPostKey = event.target.parentNode.parentNode.getAttribute("data-id");
+    // handle firebase update event
     var ref = firebase.database().ref(newPostKey);
     ref.once("value")
         .then(function(snapshot) {
+            // get the train details
             var trainName = snapshot.val().name;
             var dest = snapshot.val().dest;
             var frequency = snapshot.val().frequency;
             var st = snapshot.val().fttime;
-
+            // populate the form fields with the current details
             $("#train-name-input").val(trainName);
             $("#destination-input").val(dest);
             $("#frequency-input").val(frequency);
             $("#fttime-input").val(st);
-
+            // modify 'Save' button to 'Update'
             $("#add-train-btn").html("Update");
+            //add the data attrib to the 'Update' button
             $("#add-train-btn").attr("data-tr", snapshot.key);
 
         });
